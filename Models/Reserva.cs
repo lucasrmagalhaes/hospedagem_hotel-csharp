61cdcace5b64dfc682eb3122864de727db3f2993
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-25 10:36:08 -0300

    Desafio Finalizado

---
+++ Models/Reserva.cs
@@ -0,0 +1,60 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count) {
+                Hospedes = hospedes;
+                
+            } else {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Capacidade menor do que o número de hóspedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10) {
+                valor -= Decimal.Divide(Decimal.Multiply(valor, 10), 100);
+            }
+
+            return valor;
+        }
+    }
+
+    class Excecao : Exception
+    {
+        public Excecao(string mensagem) : base(mensagem)
+        {
+            // Construtor que chama o construtor da classe genérica
+        }
+    }
+}
